fa642da982f40585374f5e3a49ca2377e146afe2
Your Name <you@example.com>
2021-04-01 21:25:17 +0700

    ccc

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,6 +10,9 @@
                             class="brand_logo" alt="Logo">
                     </div>
                 </div>
+                <div class="alert alert-success">
+  <strong>Success!</strong> Indicates a successful or positive action.
+</div>
                 <div class="d-flex justify-content-center form_container">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
